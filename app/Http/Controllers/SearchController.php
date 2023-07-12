b8d050681d0247bcb5c078696fa1c6d045c4ab99
kary <39820@ufp.edu.pt>
2023-07-12 19:50:34 +0100

    finished just missing bots

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Car; // Replace with your model name
+use App\Models\CarPhoto; // Replace with your model name
 
 class SearchController extends Controller
 {
@@ -14,17 +15,17 @@ class SearchController extends Controller
         $yearMax = $request->input('yearMax');
         $mileageMin = $request->input('mileageMin');
         $mileageMax = $request->input('mileageMax');
-        $priceMin = $request->input('priceMin');
-        $priceMax = $request->input('priceMax');
-    
+        $PriceMin = $request->input('PriceMin');
+        $PriceMax = $request->input('PriceMax');
+
         // Start with the base query
         $results = Car::where('brand', 'ILIKE', "%{$query}%");
-    
+
         // Check if brand results are empty, then search by model
         if ($results->count() === 0) {
             $results = Car::where('model', 'ILIKE', "%{$query}%");
         }
-    
+
         // Add conditions for additional parameters if they are provided
         if ($yearMin && $yearMax) {
             $results = $results->whereBetween('year', [$yearMin, $yearMax]);
@@ -33,7 +34,7 @@ class SearchController extends Controller
         } else if ($yearMax) {
             $results = $results->where('year', '<=', $yearMax);
         }
-    
+
         if ($mileageMin && $mileageMax) {
             $results = $results->whereBetween('mileage', [$mileageMin, $mileageMax]);
         } else if ($mileageMin) {
@@ -41,22 +42,33 @@ class SearchController extends Controller
         } else if ($mileageMax) {
             $results = $results->where('mileage', '<=', $mileageMax);
         }
-    
-        if ($priceMin && $priceMax) {
-            $results = $results->whereBetween('price', [$priceMin, $priceMax]);
-        } else if ($priceMin) {
-            $results = $results->where('price', '>=', $priceMin);
-        } else if ($priceMax) {
-            $results = $results->where('price', '<=', $priceMax);
+
+        if ($PriceMin && $PriceMax) {
+            $results = $results->whereBetween('price', [$PriceMin, $PriceMax]);
+        } else if ($PriceMin) {
+            $results = $results->where('price', '>=', $PriceMin);
+        } else if ($PriceMax) {
+            $results = $results->where('price', '<=', $PriceMax);
         }
-    
+
         // Retrieve the search results
         $results = $results->get();
-    
-        return view('search_results', ['results' => $results]);
+
+        // Create an empty array to store car IDs
+        $carIds = [];
+
+        foreach ($results as $car) {
+            $carIds[] = $car->id;
+        }
+
+        // Retrieve all photos associated with the cars
+        $car_photos = CarPhoto::whereIn('car_id', $carIds)->get();
+
+        return view('search_results', ['results' => $results, 'car_photos' => $car_photos]);
+
     }
-    
-    
+
+
 
     public function suggest(Request $request)
     {
--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -1,4 +1,159 @@
-@foreach ($results as $result)
-    <p>{{ $result }}</p>
-    <!-- Display other relevant information -->
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+    <link rel="stylesheet" href="{{ asset('css/show_results.css') }}">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+    <div class="wrapper">
+      <div class="label">Pesquisa</div>
+      <form action="{{ route('search') }}" method="GET" class="searchBar">
+        <div class="input-group">
+          <div class="search-input">
+            <input id="searchQueryInput" type="text" name="query" placeholder="Search" value="" oninput="suggest()" />
+            <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+              </svg>
+            </button>
+          </div>
+        <div id="suggestionsDropdown"></div>
+        </div>
+        <div class="input-group">
+          <div class="range-input">
+            <input type="number" name="yearMin" placeholder="Min Year" />
+            <input type="number" name="yearMax" placeholder="Max Year" />
+          </div>
+          <div class="range-input">
+            <input type="number" name="mileageMin" placeholder="Min Mileage" />
+            <input type="number" name="mileageMax" placeholder="Max Mileage" />
+          </div>
+          <div class="range-input">
+            <input type="number" name="PriceMin" placeholder="Min Price" />
+            <input type="number" name="PriceMax" placeholder="Max Price" />
+          </div>
+        </div>
+      </form>
+    </div>
+
+
+    
+    <div class="car-showcase">
+      <div class="car-grid">
+          @foreach ($results as $result)
+          <div class="car-card">
+              <a href="{{ route('car', ['id' => $result->id]) }}">
+                  <div id="car-carousel-{{ $result->id }}" class="carousel slide" data-bs-slide="prev">
+                      <div class="carousel-inner">
+                          @php $firstPhoto = true; @endphp
+                          @foreach ($car_photos as $car_photo)
+                              @if ($car_photo->car_id == $result->id)
+                              <div class="carousel-item{{ $firstPhoto ? ' active' : '' }}">
+                                  <div class="image-container">
+                                      <img src="{{ $car_photo->photo_path }}" alt="Car Photo" class="car-image">
+                                  </div>
+                              </div>
+                              @php $firstPhoto = false; @endphp
+                              @endif
+                          @endforeach
+                      </div>
+                      <button class="carousel-control-next" type="button" data-bs-target="#car-carousel-{{ $result->id }}" data-bs-slide="next"></button>
+                  </div>
+                  <div class="car-details">
+                      <h3 class="car-title">{{ $result->brand }}</h3>
+                      <div class="car-info">
+                          <div class="car-model">{{ $result->model }}</div>
+                          <div class="car-extra">
+                              <span class="car-mileage">{{ number_format($result->mileage, 0, ',', '.') }}Km</span>
+                              <span class="car-extra-icon">●</span>
+                              <span class="car-publication">{{ $result->year }}</span>
+                              <span class="car-extra-icon">●</span>
+                              <span class="car-fuel">{{ $result->fuelType }}</span>
+                          </div>
+                      </div>
+                      <div class="car-price">{{ $result->price }}€</div>
+                  </div>
+              </a>
+          </div>
+          @endforeach
+      </div>
+  </div>
+  
+  <script>
+      $(document).ready(function() {
+          $('.car-image').hover(
+              function() {
+                  var $carousel = $(this).closest('.carousel');
+                  var $nextButton = $carousel.find('.carousel-control-next');
+                  setTimeout(function() {
+                    $nextButton.trigger('click');
+                  }, 500);
+              }
+          );
+      });
+  </script>
+  
+  
+
+    <script>
+
+// Attach an event listener to the input field
+function suggest() {
+  const searchInput = document.querySelector('#searchQueryInput');
+  const suggestionsDropdown = document.querySelector('#suggestionsDropdown');
+
+  const query = searchInput.value.trim(); // Trim leading and trailing spaces
+
+  // Send an AJAX request to fetch suggested terms
+  fetch(`/search/suggest?query=${query}`)
+    .then(response => response.json())
+    .then(suggestedTerms => {
+      // Clear previous suggestions
+      suggestionsDropdown.innerHTML = '';
+
+      if (query !== "" && suggestedTerms.length > 0) {
+        // Loop through the suggested terms and create dropdown items
+        suggestedTerms.forEach(term => {
+          const suggestionItem = document.createElement('div');
+          suggestionItem.classList.add('dropdown-item');
+          suggestionItem.textContent = term;
+
+          // Add click event listener to suggestion item
+          suggestionItem.addEventListener('click', () => {
+            // Set the clicked suggestion as the search query
+            searchInput.value = term;
+
+            // Trigger the form submission
+            //document.querySelector('.searchBar').submit();
+          });
+
+          suggestionsDropdown.appendChild(suggestionItem);
+        });
+
+        // Show the suggestions dropdown
+        suggestionsDropdown.style.display = 'block';
+      } else {
+        // Hide the suggestions dropdown when there are no suggested terms or the input value is empty
+        suggestionsDropdown.style.display = 'none';
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// Hide suggestions dropdown when clicking outside
+document.addEventListener('click', function(event) {
+  const searchInput = document.querySelector('#searchQueryInput');
+  const suggestionsDropdown = document.querySelector('#suggestionsDropdown');
+
+  if (event.target !== searchInput && event.target !== suggestionsDropdown) {
+    suggestionsDropdown.style.display = 'none';
+  }
+});
+  </script>
+
+
+@endsection
